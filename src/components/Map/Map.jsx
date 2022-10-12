7dfc9d43f13b96bb48e29c8080fe2c566c056e00
wazzywael <wael.k@hotmail.co.uk>
2022-10-12 23:30:33 +0100

    Map component added to main app

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery, styled } from "@mui/material";
+import LocationOnOutlinedIcon from "@mui/icons-material";
+import Rating from "@mui/lab";
 
 const Map = () => {
-  return <h1>Map</h1>;
+  const CustomizedTypography = styled(Typography)`
+  paper: {
+    padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
+  },
+  mapContainer: {
+    height: '85vh', width: '100%',
+  },
+  markerContainer: {
+    position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },
+  },
+  pointer: {
+    cursor: 'pointer',
+  },
+  `;
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
+  return (
+    <CustomizedTypography>
+      <div className="mapContainer">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyD4McmYUyGsCNQ8D-TW7Mrs-MO_A6pB4w8" }}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          options={""}
+          onChange={""}
+          onChildClick={""}
+        ></GoogleMapReact>
+      </div>
+    </CustomizedTypography>
+  );
 };
 
 export default Map;
